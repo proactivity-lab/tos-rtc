5599cf846e63770db389d509c9eb9206345ef421
Raido Pahtma <raidopahtma@gmail.com>
2016-06-27 10:29:06 +0300

    Added yxktime_reverse function.

--- tos/time64.h
+++ tos/time64.h
@@ -37,22 +37,37 @@ struct tm* gmtime_r(const time64_t* timep, struct tm* result) @C();
 // Add/subtract seconds from a struct tm
 struct tm* tm_add_seconds(struct tm* tm, time64_t seconds) @C();
 
+void yxkzero(struct tm* yxk) {
+	yxk->tm_sec = 0;
+	yxk->tm_min = 0;
+	yxk->tm_hour = 0;
+	yxk->tm_mday = 1;
+	yxk->tm_mon = 0;
+	yxk->tm_year = ((yxk->tm_year) / 100) * 100; // Beginning of century, 2000-01-01 00:00:00 = 946684800
+	yxk->tm_wday = 0; // Does not really matter
+	yxk->tm_yday = 0; // Does not really matter
+}
+
 uint32_t yxktime(time64_t* t) {
 	if(*t != (time64_t)(-1)) {
 		struct tm yxk;
 		if(gmtime_r(t, &yxk) != NULL) {
-			yxk.tm_sec = 0;
-			yxk.tm_min = 0;
-			yxk.tm_hour = 0;
-			yxk.tm_mday = 1;
-			yxk.tm_mon = 0;
-			yxk.tm_year = ((yxk.tm_year) / 100) * 100; // Beginning of century, 2000-01-01 00:00:00 = 946684800
-			yxk.tm_wday = 0; // Does not really matter
-			yxk.tm_yday = 0; // Does not really matter
+			yxkzero(&yxk);
 			return (uint32_t)(*t - mktime(&yxk));
 		}
 	}
 	return UINT32_MAX;
+}
+
+time64_t yxktime_reverse(uint32_t yxks, time64_t* now) {
+	if(*now != (time64_t)(-1)) {
+		struct tm yxk;
+		if(gmtime_r(now, &yxk) != NULL) {
+			yxkzero(&yxk);
+			return mktime(&yxk) + yxks;
+		}
+	}
+	return -1;
 }
 
 #endif // TIME64_H_
